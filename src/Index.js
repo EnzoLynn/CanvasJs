cf3d8aace763996947acb56fd185becbcdd977af
chenheling <6710582@qq.com>
2016-06-28 14:47:09 +0800

    6.28

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,34 @@
-//Create a stage by getting a reference to the canvas
-stage = new createjs.Stage("demoCanvas");
-//Create a Shape DisplayObject.
-circle = new createjs.Shape();
-circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-//Set position of Shape instance.
-circle.x = circle.y = 50;
-//Add Shape instance to stage display list.
-stage.addChild(circle);
-//Update stage will render next frame
-stage.update();
+var init = function() {
+    //Create a stage by getting a reference to the canvas
+    var stage = new createjs.Stage("demoCanvas");
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+    stage.update();
+
+    function handleClick(event) {
+        // Click happenened
+        console.log("click");
+    }
+    stage.addEventListener("click", handleClick);
+
+
+    function handlePress(event) {
+        // A mouse press happened.
+        // Listen for mouse move while the mouse is down:
+        console.log("press");
+
+
+    }
+    stage.addEventListener("mousedown", handlePress);
+
+
+
+    function handleMove(event) {
+        console.log("move");
+        // Check out the DragAndDrop example in GitHub for more
+    }
+    stage.addEventListener("pressmove", handleMove);
+}
