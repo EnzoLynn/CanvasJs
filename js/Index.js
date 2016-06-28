2f8406beb202b92ffeab2e48fdb81faeff619542
chenheling <6710582@qq.com>
2016-06-28 15:14:28 +0800

    6.28

--- js/Index.js
+++ js/Index.js
@@ -4,7 +4,7 @@ var init = function init() {
     //Create a stage by getting a reference to the canvas
     var stage = new createjs.Stage("demoCanvas");
     var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.graphics.beginFill("DeepSkyBlue").drawRect(0, 0, 100, 100); //.drawCircle(0, 0, 50);
     circle.x = 100;
     circle.y = 100;
     stage.addChild(circle);
@@ -28,4 +28,17 @@ var init = function init() {
         // Check out the DragAndDrop example in GitHub for more
     }
     stage.addEventListener("pressmove", handleMove);
+
+    var angle = 0;
+    function tick(event) {
+        console.log("tick");
+        angle += 1;
+        var value = 12 * angle;
+
+        circle.rotation = value;
+        //circle.scaleX = circle.scaleY = value / 360;
+        stage.update(event);
+        // Check out the DragAndDrop example in GitHub for more
+    }
+    createjs.Ticker.addEventListener("tick", tick);
 };
