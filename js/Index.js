0950d125e11111d166c00cd3734dc0d31f1b0273
chenheling <6710582@qq.com>
2016-06-30 09:53:03 +0800

    6.30

--- js/Index.js
+++ js/Index.js
@@ -3,11 +3,21 @@
 var init = function init() {
     //Create a stage by getting a reference to the canvas
     var stage = new createjs.Stage("demoCanvas");
+
     var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawRect(0, 0, 100, 100); //.drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
+    circle.graphics.beginFill("#E11010").drawCircle(150, 150, 150);
     stage.addChild(circle);
+
+    var rect = new createjs.Shape();
+    rect.graphics.beginFill("DeepSkyBlue").drawRect(0, 0, 100, 100); //.drawCircle(0, 0, 50);
+    rect.x = 150;
+    rect.y = 150;
+    stage.addChild(rect);
+
+    var s = new createjs.Shape();
+    s.graphics.setStrokeStyle(16, "round", "round").beginFill("#0F0").beginStroke("#f90").moveTo(20, 10).lineTo(90, 90).lineTo(90, 140);
+    stage.addChild(s);
+
     stage.update();
 
     function handleClick(event) {
@@ -30,12 +40,13 @@ var init = function init() {
     stage.addEventListener("pressmove", handleMove);
 
     var angle = 0;
+
     function tick(event) {
         console.log("tick");
         angle += 1;
         var value = 12 * angle;
 
-        circle.rotation = value;
+        rect.rotation = value;
         //circle.scaleX = circle.scaleY = value / 360;
         stage.update(event);
         // Check out the DragAndDrop example in GitHub for more
