69226f2bd034e578e6a00ede9f03ee6dce1c347c
chenheling <6710582@qq.com>
2016-07-06 16:04:13 +0800

    Line

--- js/SkillsTree.js
+++ js/SkillsTree.js
@@ -1,26 +1,64 @@
-"use strict";
+'use strict';
 
 define(function (require, exports, module) {
-	var stage, container;
-
+	var stage;
+	var skills = {
+		"自动构建": ['Grunt', 'Glup', 'Webpack'],
+		"自动编译": ['Babel', 'Less', 'Sass', 'Concat', 'Uglify', 'Cssmin', 'Copy', 'Jshint'],
+		"编程语言": ['JavaScript', 'Html/Html5/DHtml', 'Css', 'NodeJs', 'C#'],
+		"UI框架": ['Extjs', 'ThreeJs', 'MUI', 'Bootstrap', 'Framwork', 'SUI', 'Telerik'],
+		"工具框架": ['ReactJs', 'React-Native', 'Handlebars', 'Jquery', 'Html5plus', 'Express'],
+		"MVC框架": ['AngularJs', 'Backbone', 'EntityFramework'],
+		"加载器": ['Seajs', 'RequireJs'],
+		"数据库": ['SqlServer', 'Mongodb', 'Access'],
+		"服务器": ['IIS', 'Nginx', 'Apathe'],
+		"操作系统": ['Window', 'Windowserver', 'Linux', 'Centos'],
+		"代码管理": ['Git', 'Svn', 'TFS'],
+		"IDE/工具": ['Sublime', 'VisualStudio', 'Eclipse', 'Phtoshop', 'Coredraw', 'MsOffice'],
+		"交互/协议": ['Json', 'Xml', 'Ajax', 'Http/Https', 'Tcp/IP', 'UDP']
+	};
 	var SkillsTree = React.createClass({
-		displayName: "SkillsTree",
+		displayName: 'SkillsTree',
 
-		init: function init() {
-			var me = this;
-			stage = new createjs.Stage(me.refs.skillTree);
-
-			container = new createjs.Container();
-			container.x = 0;
-			container.y = 0;
+		createSkill: function createSkill(x, y) {
+			var container = new createjs.Container();
+			container.x = x || 0;
+			container.y = y || 0;
 
 			var rect = new createjs.Shape();
 			rect.graphics.beginFill("DeepSkyBlue").drawRect(0, 0, 100, 30); //.drawCircle(0, 0, 50);
 			container.addChild(rect);
 
-			stage.addChild(container);
+			var line = new createjs.Shape();
+			line.graphics.beginStroke("#D60707").moveTo(0, 0).lineTo(0, 10); //.drawCircle(0, 0, 50);
+			line.x = 50;
+			line.y = 30;
+			container.addChild(line);
+
+			return {
+				container: container,
+				line: line
+			};
+		},
+		init: function init() {
+			var me = this;
+			stage = new createjs.Stage(me.refs.skillTree);
+
+			var skill = me.createSkill();
+			var skill2 = me.createSkill(110, 50);
+
+			stage.addChild(skill.container);
+			stage.addChild(skill2.container);
 
 			stage.update();
+			//createjs.Ticker.setFPS(20);
+			createjs.Ticker.addEventListener("tick", stage);
+			createjs.Tween.get(skill.line, {
+				loop: false
+			}).to({
+				scaleX: 3,
+				scaleY: 4
+			}, 1000 * 10, createjs.Ease.quintOut);
 		},
 		componentDidMount: function componentDidMount() {
 			var me = this;
@@ -28,9 +66,9 @@ define(function (require, exports, module) {
 		},
 		render: function render() {
 			return React.createElement(
-				"div",
-				{ className: "skills" },
-				React.createElement("canvas", { ref: "skillTree", className: "demoCanvas", width: "400", height: "400" })
+				'div',
+				{ className: 'skills' },
+				React.createElement('canvas', { ref: 'skillTree', className: 'demoCanvas', width: '400', height: '400' })
 			);
 		}
 	});
